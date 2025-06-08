0c71358c6f5bc29d20cdec87136fc1c235b16d09
rtzh1ran <ratuzhiran@gmail.com>
2025-06-08 08:48:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ st.title("Selamat datang di website ini")
 st.write(
     "ngodingseru bersama Bapak Hendri"
 )
-st.image(IMG-20250607-0107 .jpg, width=200)
+st.image("IMG-20250607-0107 .jpg", width=200)
